a8c9ce394d46941cd5e239dc51badf35f02229ec
jun.jiang <jun.jiang@anker.io>
2024-06-18 18:45:10 +0800

    初始化

---
+++ api/test.js
@@ -0,0 +1,12 @@
+const Router=require('koa-router');
+const router=new Router;
+
+router.get('/',(ctx,next)=>{
+    ctx.body='Hello World'
+})
+
+router.post('/users',(ctx,next)=>{
+    ctx.body='here is users'
+})
+
+module.exports=router
---
+++ app.js
@@ -0,0 +1,16 @@
+const Koa=require('koa')
+const app=new Koa()
+const router=require('./api/test')
+
+app.use(async (ctx,next)=>{
+    const start=Date.now();
+    await next()
+    const ms=Date.now()-start;
+    console.log(`${ctx.method} ${ctx.url} ${ms}ms`)
+})
+
+app.use(router.routes())
+
+app.listen(9090,()=>{
+    console.log(`serve run localhost:9090`)
+})
